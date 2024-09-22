2ab7b3f9abdda0940ca2333d9cd13deb4cb3406a
Diaa-Dw <oava9944@gmail.com>
2024-09-22 22:02:31 +0300

    SETUP react project and create header component

---
+++ src/components/Header.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <header>
+      <div class='logo'>
+        <img src='logo.png' alt='Company Logo' />
+      </div>
+      <div class='header-text'>
+        <h1>Our Products</h1>
+        <p>
+          Browse through our wide range of products. Use the filter below to
+          narrow down your choices.
+        </p>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
